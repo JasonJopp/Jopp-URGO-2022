d0b639ab9d35c56d1ca2d7af88612541f0f33abb
JasonJopp <jmjopp@protonmail.com>
2022-06-24 11:01:12 -0500

    Updated comments

--- RL/drive.py
+++ RL/drive.py
@@ -1,8 +1,19 @@
+# -----------------------------------------------------------------------------
+# Code Purpose: Given a drive command, move a Sphero Rover using the command.
+#
+# NOTE: Right now driver() uses the same speed for both left & right tracks,
+# this is not a requirement. So if you want you can add left/right speed params
+# and trying to implement gradual turning while moving forward/back.
+#
+# Author(s): Jason Jopp, <your name here>
+# -----------------------------------------------------------------------------
+
+
 import asyncio, numpy as np, time
 from sphero_sdk import RawMotorModesEnum, Colors
 
 async def driver(rvr, leftMode, rightMode, driveTime = 2, 
-    speed = 64, cmdName = "Unnamed Command", colorCode = [255,255,255]) -> None:
+    speed = 64, cmdName = "Unnamed Command", colorCode = [255,255,255]):
     """
     Drives a given RVR in a given direction.
     Params: rover obj, left track drive mode (0,1,2), 
@@ -10,42 +21,45 @@ async def driver(rvr, leftMode, rightMode, driveTime = 2,
     NOTE: Right/Left modes: 0 = stop, 1 = forward, 2 = reverse
     """
 
-    # Various checks on drive variables
+    # Various checks on drive variables, must fall within these bounds to work
     if not (0 <= speed <= 255):
-        print("DRIVE VARROR: Invalid speed entered, must be between 0 and 255.")
+        print("ERROR: Invalid speed entered, must be between 0 and 255.")
         exit()
 
-    if not (driveTime > 0): # It is difficult to move a rover for negative secs.
-        print("DRIVE VARROR: Invalid time entered, must be greater than zero.")
+    if not (driveTime > 0):
+        print("ERROR: Invalid time entered, must be greater than zero.")
         exit()
 
     if not (0 <= leftMode <= 2) and isinstance(leftMode, int):
-        print("DRIVE VARROR: lMode was not an int between 0-2, incl.")
+        print("ERROR: lMode was not an int between 0-2, incl.")
         exit()
     
     if not (0 <= rightMode <= 2) and isinstance(rightMode, int):
-        print("DRIVE VARROR: lMode was not an int between 0-2, incl.")
+        print("ERROR: lMode was not an int between 0-2, incl.")
         exit()
 
-    # Used to drive the rover forward a certain amount of time, rover acts in
-    # two second increments for UNK reason, this way the last movement can be
-    # cut off at the 'finalTime' amount, with 'amountTimes' being how many 2
-    # second rotations are required before that.
 
-    # NOTE: This may make rover pause between commands, but I don't know how to
-    # interrupt the RVR's movement without a stop command, which lasts two seconds.
+    # Divides driveTime between amountTimes and finalTime, rover acts in two
+    # second increments for UNK reason, this way the last movement can be
+    # cut off at the 'finalTime' amount, with 'amountTimes' being how many 2
+    # second rotations are required before that to meet the required driveTime.
+    # This causes rover pause between commands, but without it the rover always
+    # moves in increments of two seconds when using the raw motor input mode.
     amountTimes = int(np.floor(driveTime/2))
     finalTime = round(driveTime%2, 3)
 
+    # Wakes the RVR from sleep mode
     rvr.wake()
 
-    # Gives the RVR time to wake up. Inconsistent without wake period.
+    # Gives RVR time to wake up, RVR sometimes misses commands without this.
     await asyncio.sleep(1)
     
     rvr.reset_yaw()
     
-    # Sets RVR leds to green when moving, unless other color code given
+    # Sets RVR leds to white when moving, unless other color code given
     rvr.led_control.set_all_leds_rgb(*colorCode)
+
+    
     while amountTimes > 0:   
         rvr.raw_motors(
             left_mode=leftMode,
@@ -56,7 +70,7 @@ async def driver(rvr, leftMode, rightMode, driveTime = 2,
         await asyncio.sleep(2)
         amountTimes -= 1
     
-    # Performs additional movement if movetime wasn't divisible by two.
+    # Performs additional movement if driveTime is greater/less than 2 seconds
     if finalTime > 0:   
         rvr.raw_motors(
             left_mode=leftMode,
@@ -72,7 +86,7 @@ async def driver(rvr, leftMode, rightMode, driveTime = 2,
             right_mode=0,
             right_duty_cycle=0
         )
-    # Allows RVR to stop before taking a photo for analysis
+    # Gives RVR time to stop before taking a photo, otherwise image is blurry
     time.sleep(.15)
-    # Sets rover leds to white, waiting state
+    # Sets rover leds to white, default waiting state
     rvr.led_control.set_all_leds_rgb(red=255, green=255, blue=255)
